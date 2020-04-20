73937a6f421eb390da32c205c723d6666e1c3e68
mistermboy <pabloyo97@hotmail.com>
2020-04-20 14:05:53 +0100

    Updates

---
+++ precache-manifest.9b84f44a12b556150bd18f639730da8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "709d269b3e778b2af051599cb267a4b8",
+    "url": "/wiki-author/index.html"
+  },
+  {
+    "revision": "b7984d7487968486bb08",
+    "url": "/wiki-author/static/css/2.55d782e9.chunk.css"
+  },
+  {
+    "revision": "c8665717c27d410e5452",
+    "url": "/wiki-author/static/css/main.5718ddd6.chunk.css"
+  },
+  {
+    "revision": "b7984d7487968486bb08",
+    "url": "/wiki-author/static/js/2.5fb81a04.chunk.js"
+  },
+  {
+    "revision": "c8665717c27d410e5452",
+    "url": "/wiki-author/static/js/main.6e44d9b7.chunk.js"
+  },
+  {
+    "revision": "4d38061ca3a7e834b720",
+    "url": "/wiki-author/static/js/runtime-main.a52a0883.js"
+  }
+]);
